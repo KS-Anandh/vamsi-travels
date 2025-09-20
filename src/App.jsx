a86b373beb7b70ad3d487c0c945da3e0fea40cb5
KS Anandh <nandhaanandh1432@gmail.com>
2025-09-20 23:28:45 +0530

    v3 updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react'
+import React, { useEffect, useRef } from 'react'
 import Navigation from './components/Navigation'
 import Slider from './components/Slider'
 import WhyToChooseUS from './components/WhyToChooseUS'
